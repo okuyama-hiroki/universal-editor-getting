a32adc6cc6dfa5bf529e5849c31e9b6cffebf9fa
Okuyama Hiroki <tos02106@adobe.com>
2026-06-02 14:34:45 +0900

    cssの追加修正

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,7 +1,7 @@
 export default function decorate(block) {
-    const [quoteWrapper] = block.children;
+  const [quoteWrapper] = block.children;
 
-    const blockquote = doqument.createElement('blockquote');
-    blockquote.textContent = quoteWrapper.textContent.trim();
-    quoteWrapper.replaceWith(blockquote);
+  const blockquote = document.createElement('blockquote');
+  blockquote.textContent = quoteWrapper.textContent.trim();
+  quoteWrapper.replaceWith(blockquote);
 }
